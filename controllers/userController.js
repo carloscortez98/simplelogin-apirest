0912a9a4be65dab3bbbabcb6c374fed49d365e30
carloscortez98 <carloscor998@gmail.com>
2022-03-04 21:10:20 -0300

    Arreglo en Controlador y package

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const usersModel = require('../models/userModel');
+const userModel = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 require('dotenv/config');
